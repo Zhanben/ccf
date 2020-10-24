9bdaca3f052ff7ec58eed7747564e289512205bb
zhang ben <ncuzhangben@outlook.com>
2020-10-24 12:13:47 +0800

    2.4.1温度转换

---
+++ 1001.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>  //用来控制cin，cout的输入输出格式的库
+using namespace std;
+
+int main(){
+	float c, f;
+	cin >> f;
+	c = (f-32)*5/9; //转换公式
+	//cout<<setiosflags(ios::fixed);
+	cout <<fixed<<setprecision(4) << c <<endl; //先设置精度，再输出
+	return 0;
+}
